bd4e04e1cccbfdeeb4e9e76cead7c6722426c4e3
Amr Salama <amr.mo.salama@gmail.com>
2020-01-16 18:13:11 +0200

    feat: codeforces null dom element error

--- src/judges/Codeforces/Codeforces.ts
+++ src/judges/Codeforces/Codeforces.ts
@@ -1,5 +1,8 @@
 import Judge from "../Judge";
-import { InvalidLoginCredentialsError } from "../../errors";
+import {
+  InvalidLoginCredentialsError,
+  NullDOMElementError
+} from "../../errors";
 import { IJudgeCredentials } from "../interfaces";
 
 class Codeforces extends Judge {
@@ -10,25 +13,37 @@ class Codeforces extends Judge {
       waitUntil: "domcontentloaded"
     });
 
-    await loginPage.evaluate(
-      ({ username, password }) => {
-        const qs = document.querySelector.bind(document);
-        const usernameInput = qs("#handleOrEmail");
-        const passwordInput = qs("#password");
-        const rememberCheckbox = qs("#remember");
-        const submitBtn = qs("input[type='submit']");
+    try {
+      await loginPage.evaluate(
+        ({ username, password }) => {
+          return new Promise((resolve, reject) => {
+            const qs = document.querySelector.bind(document);
+            const usernameInput = qs("#handleOrEmail");
+            const passwordInput = qs("#password");
+            const rememberCheckbox = qs("#remember");
+            const submitBtn = qs("input[type='submit']");
 
-        if (usernameInput && passwordInput && rememberCheckbox && submitBtn) {
-          (usernameInput as HTMLInputElement).value = username;
-          (passwordInput as HTMLInputElement).value = password;
-          (rememberCheckbox as HTMLInputElement).click();
-          (submitBtn as HTMLInputElement).click();
-        } else {
-          // TODO: handle possibly null elements due to codeforces changes
-        }
-      },
-      { ...credentials }
-    );
+            if (
+              usernameInput &&
+              passwordInput &&
+              rememberCheckbox &&
+              submitBtn
+            ) {
+              (usernameInput as HTMLInputElement).value = username;
+              (passwordInput as HTMLInputElement).value = password;
+              (rememberCheckbox as HTMLInputElement).click();
+              (submitBtn as HTMLInputElement).click();
+              resolve();
+            } else {
+              reject();
+            }
+          });
+        },
+        { ...credentials }
+      );
+    } catch (error) {
+      throw new NullDOMElementError("Codeforces");
+    }
 
     await new Promise(resolve => loginPage.once("domcontentloaded", resolve));
 
